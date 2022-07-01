acc796b4830e0512f74629699227b662174e19fe
HungDuong <hungduongg2909@gmail.com>
2022-07-01 21:56:01 +0700

    format code with prettier

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 import logo from './logo.svg';
-import Button from '~/components/index'
+import Button from '~/components/index';
 import './App.css';
-
 function App() {
-  return ( <
-    div className = "App" >
-    <
-    Button / >
-    <
-    header className = "App-header" >
-    <
-    img src = {
-      logo
-    }
-    className = "App-logo"
-    alt = "logo" / >
-    <
-    p >
-    Edit < code > src / App.js < /code> and save to reload. <
-    /p> <
-    a className = "App-link"
-    href = "https://reactjs.org"
-    target = "_blank"
-    rel = "noopener noreferrer" >
-    Learn React <
-    /a> <
-    /header> <
-    /div>
-  );
+    return (
+        <div className="App">
+            <Button />
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
