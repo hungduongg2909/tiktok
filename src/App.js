8f13748ef1a1b333593337240f60daf5682a729b
HungDuong <hungduongg2909@gmail.com>
2022-07-04 13:48:03 +0700

    init complete base

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+import { HeaderOnly } from '~/components/Layout';
+import { Fragment } from 'react';
+
 function App() {
     return (
-        <div className="App">
-            <h1>React App</h1>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+
+                        let Layout;
+                        switch (route.layout) {
+                            case null:
+                                Layout = Fragment;
+                                break;
+                            case HeaderOnly:
+                                Layout = HeaderOnly;
+                                break;
+                            default:
+                                Layout = DefaultLayout;
+                                break;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
